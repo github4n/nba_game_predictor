0d4627fa07c53298e630fd100232f2ca58d7d86a
sarahkurd <skurdogh@calpoly.edu>
2018-12-04 14:58:05 -0800

    scraper in class and function

--- scraping.py
+++ scraping.py
@@ -5,99 +5,86 @@ import json
 import nba_py
 import datetime
 
-# scrape game id's for all NBA season games /
+# class NBAScraper defines scraping function
+# to get various game data tables from nba stats website
 # from 1989-1990 season to 2017-2018 season
 # url for getting your user agent: https://stackoverflow.com/questions/46781563/how-to-obtain-a-json-response-from-the-stats-nba-com-api
 
-# query to get game id from each row
-def getIds(row):
-	return row['resultSets'][1]['rowSet'][0][2]
+class NBAScraper:
 
-# range of dates
-dates = pd.date_range(start='11/03/1989', end='11/12/2017', freq='D').strftime('%m/%d/%y')
-
-# json_data = []
-
-# DataFrame column labels
-ingame_columns = 	['GAME_DATE_EST', 'GAME_SEQUENCE', 'GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'TEAM_CITY_NAME', 'TEAM_WINS_LOSSES', 'PTS_QTR1',
+	# DataFrame column labels
+	ingame_columns = 	['GAME_DATE_EST', 'GAME_SEQUENCE', 'GAME_ID', 'TEAM_ID','TEAM_ABBREVIATION', 'TEAM_CITY_NAME', 'TEAM_WINS_LOSSES', 'PTS_QTR1',
 					'PTS_QTR2', 'PTS_QTR3','PTS_QTR4', 'PTS_OT1','PTS_OT2','PTS_OT3','PTS_OT4', 'PTS_OT5', 'PTS_OT6', 'PTS_OT7','PTS_OT8', 'PTS_OT9',
 					'PTS_OT10', 'PTS','FG_PCT',	'FT_PCT','FG3_PCT',	'AST','REB','TOV']
 
-meta_columns = ['GAME_DATE_EST', 'GAME_SEQUENCE', 'GAME_ID', 'GAME_STATUS_ID',
+	meta_columns = ['GAME_DATE_EST', 'GAME_SEQUENCE', 'GAME_ID', 'GAME_STATUS_ID',
 				'GAME_STATUS_TEXT', 'GAMECODE', 'HOME_TEAM_ID','VISITOR_TEAM_ID', 'SEASON',
 				'LIVE_PERIOD', 'LIVE_PC_TIME', 'NATL_TV_BROADCASTER_ABBREVIATION', 'LIVE_PERIOD_TIME_BCAST', 'WH_STATUS']
 
-series_columns = ['GAME_ID', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'GAME_DATE_EST',
+	series_columns = ['GAME_ID', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'GAME_DATE_EST',
 				 'HOME_TEAM_WINS', 'HOME_TEAM_LOSSES', 'SERIES_LEADER']
 
-last_meeting_columns = ['GAME_ID', 'LAST_GAME_ID', 'LAST_GAME_DATE_EST', 'LAST_GAME_HOME_TEAM_ID',
+	last_meeting_columns = ['GAME_ID', 'LAST_GAME_ID', 'LAST_GAME_DATE_EST', 'LAST_GAME_HOME_TEAM_ID',
 						'LAST_GAME_HOME_TEAM_CITY', 'LAST_GAME_HOME_TEAM_NAME', 'LAST_GAME_HOME_TEAM_ABBREVIATION',
 						'LAST_GAME_HOME_TEAM_POINTS', 'LAST_GAME_VISITOR_TEAM_ID', 'LAST_GAME_VISITOR_TEAM_CITY',
 						'LAST_GAME_VISITOR_TEAM_NAME', 'LAST_GAME_VISITOR_TEAM_CITY1', 'LAST_GAME_VISITOR_TEAM_POINTS']
 
-# These empty dataframe are repeatedly concatenated in the for loop below with the information from each days games
-nba_ingame_stats = pd.DataFrame(columns = ingame_columns)
-nba_meta_stats = pd.DataFrame(columns = meta_columns)
-nba_series_stats = pd.DataFrame(columns  = series_columns)
-nba_last_meeting_stats = pd.DataFrame(columns = last_meeting_columns)
+
+	# input date range
+	def scrape(start_date, end_date):
+		# These empty dataframe are repeatedly concatenated in the for loop below with the information from each days games
+		nba_ingame_stats = pd.DataFrame(columns = NBAScraper.ingame_columns)
+		nba_meta_stats = pd.DataFrame(columns = NBAScraper.meta_columns)
+		nba_series_stats = pd.DataFrame(columns  = NBAScraper.series_columns)
+		nba_last_meeting_stats = pd.DataFrame(columns = NBAScraper.last_meeting_columns)
+
+		# range of dates
+		dates = pd.date_range(start=start_date, end=end_date, freq='D').strftime('%m/%d/%y')
+		for date in dates:
+			try:
+				#headers = {'User-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Mobile Safari/537.36'}
+				headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
+				request = requests.get("https://stats.nba.com/stats/scoreboard/", 
+							params={'GameDate':date, 'LeagueID':'00', 'DayOffset':'0'}, 
+							headers=headers)
+
+				# check status code for OK response
+				print("Getting " + date)
+				print(request.status_code)
+				json = request.json()
+
+				# Meta info about game (e.g. who is hometeam)
+				todays_meta_data = json['resultSets'][0]['rowSet']
+				todays_meta_data_df = pd.DataFrame(todays_meta_data, columns=NBAScraper.meta_columns)
+				nba_meta_stats = pd.concat([nba_meta_stats,todays_meta_data_df],ignore_index=True)
+
+				# Info about the seriese between two teams
+				todays_series_info = json['resultSets'][2]['rowSet']
+				todays_series_info_df = pd.DataFrame(todays_series_info,columns = NBAScraper.series_columns)
+				nba_series_stats = pd.concat([nba_series_stats,todays_series_info_df],ignore_index=True)
+
+				# Info about the ingame statistics
+				todays_stats = json['resultSets'][1]['rowSet']
+				todays_stats_df = pd.DataFrame(todays_stats,columns=NBAScraper.ingame_columns)
+				nba_ingame_stats = pd.concat([nba_ingame_stats,todays_stats_df],ignore_index=True)
+
+				# Info about the lastgame statistics
+				todays_last_meeting_stats = json['resultSets'][3]['rowSet']
+				todays_last_meeting_stats_df = pd.DataFrame(todays_last_meeting_stats,columns=NBAScraper.last_meeting_columns)
+				nba_last_meeting_stats = pd.concat([nba_last_meeting_stats,todays_last_meeting_stats_df],ignore_index=True)
+
+			except requests.exceptions.ReadTimeout:
+				print("=(")
+
+		nba_ingame_stats.to_csv('nba_ingame.csv')
+		nba_meta_stats.to_csv('nba_meta.csv')
+		nba_series_stats.to_csv('nba_series.csv')
+		nba_last_meeting_stats.to_csv('nba_last_meeting.csv')
 
 
-for date in dates:
-	try:
-		headers = {'User-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Mobile Safari/537.36'}
-		# headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'}
-		request = requests.get("https://stats.nba.com/stats/scoreboard/", 
-					params={'GameDate':date, 'LeagueID':'00', 'DayOffset':'0'}, 
-					headers=headers)
-
-		# check status code for OK response
-		print("Getting " + date)
-		print(request.status_code)
-		json = request.json()
-		# build list of jsons for each date
-		# json_data.append(request.json())
-		# print(len(columns))
-
-		# Meta info about game (e.g. who is hometeam)
-		todays_meta_data = json['resultSets'][0]['rowSet']
-		todays_meta_data_df = pd.DataFrame(todays_meta_data, columns=meta_columns)
-		nba_meta_stats = pd.concat([nba_meta_stats,todays_meta_data_df],ignore_index=True)
-
-		# Info about the seriese between two teams
-		todays_series_info = json['resultSets'][2]['rowSet']
-		todays_series_info_df = pd.DataFrame(todays_series_info,columns = series_columns)
-		nba_series_stats = pd.concat([nba_series_stats,todays_series_info_df],ignore_index=True)
-
-		# Info about the ingame statistics
-		todays_stats = json['resultSets'][1]['rowSet']
-		todays_stats_df = pd.DataFrame(todays_stats,columns=ingame_columns)
-		nba_ingame_stats = pd.concat([nba_ingame_stats,todays_stats_df],ignore_index=True)
-
-		# Info about the lastgame statistics
-		todays_last_meeting_stats = json['resultSets'][3]['rowSet']
-		todays_last_meeting_stats_df = pd.DataFrame(todays_last_meeting_stats,columns=last_meeting_columns)
-		nba_last_meeting_stats = pd.concat([nba_last_meeting_stats,todays_last_meeting_stats_df],ignore_index=True)
-
-	except requests.exceptions.ReadTimeout:
-		print("=(")
-
-# # create numpy array out of json list
-# json_array = np.array(json_data)
-
-# # apply custom function on each row
-# id_vect = np.vectorize(getIds)
-# game_ids = id_vect(json_array)
-# print(game_ids)
-
-# # save ids in csv
-# np.savetxt('game_ids.csv', game_ids, fmt='%s', delimiter=',')
-# print("nba shape:", nba.shape)
 
 
-nba_ingame_stats.to_csv('nba_ingame.csv')
-nba_meta_stats.to_csv('nba_meta.csv')
-nba_series_stats.to_csv('nba_series.csv')
-nba_last_meeting_stats.to_csv('nba_last_meeting.csv')
+
 
 
 
